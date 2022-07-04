083877cfc67c487618259b0c928d31a2c3846b47
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-04 18:07:59 -0300

    Create aliases.ts

---
+++ Aliases/aliases.ts
@@ -0,0 +1,19 @@
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+
+type car = {
+    year: CarYear;
+    type: CarType;
+    model: CarModel;
+}
+
+const carYear: CarYear = 2021;
+const carType: CarType = "Corolla";
+const carModel: CarModel = "Toyota";
+
+const car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+}
