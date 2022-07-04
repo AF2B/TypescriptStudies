a7b39f996fd272987a5291f2af4992073c462fe5
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-04 18:10:38 -0300

    Create arraysTuples.ts

---
+++ Tuples/arraysTuples.ts
@@ -0,0 +1,7 @@
+const tuples: [string, number, boolean] = ["teste", 7, true];
+
+const readonlyTuple: readonly [string, number, boolean] = ["teste", 3, true];
+/**
+ * it's a good practice create a tuples readonly. because you have more security in your tuple.
+ * making your tuple readonly, is safety because anyone can't push whatever he want in 3+ index.
+ */
