36e0fa58757c94005bdcfda767ef1af0b8292139
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-04 18:10:23 -0300

    Create optionalProperty.ts

---
+++ ObjectTypes/optionalProperty.ts
@@ -0,0 +1,10 @@
+const car3: {type: string, year?: number} = {
+    type: "Hyundai"
+};
+
+// car3.year = "2011"; optional.
+
+/**
+ * when you use "?" in one variable, you define this to a optional variable.
+ * so... you don't need to declare a value to this variable.
+ */
