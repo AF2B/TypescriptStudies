2ab6fb3a698595db2ba46c43893a9bf7ef7a0120
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-07-04 18:10:20 -0300

    Create objType.ts

---
+++ ObjectTypes/objType.ts
@@ -0,0 +1,11 @@
+const car: {type: string, model: string, year: number} = {
+    type: "Hyundai",
+    model: "Azera",
+    year: 2020
+}
+
+const car2 = {
+    type: "Hyundai"
+}
+
+car2.type = "BYD";
